3cc21b5df1f3f0d7e62813f8d8bb3d0ef0dba380
drohreo <sohan@graceful.nu>
2015-08-05 16:09:40 +0200

    resfactored and cleaned

--- app/sfSpUtils/spPeoplePicker.directive.js
+++ app/sfSpUtils/spPeoplePicker.directive.js
@@ -1,15 +1,94 @@
 ﻿
 (function (angular) {
+    'use strict';
     var directiveId = "spPeoplePicker";
 
     function spPeoplePicker() {
         var uniqueNr = 1;
+        //utility functions
+        function DeleteProcessedUser(a) {
+            var e = null;
+            if (a == null) {
+                var c = document.getElementById(this.ResolvedListElementId);
+                if (c != null)
+                    a = c.lastChild
+            }
+            if (a != null) {
+                var b = a.id;
+                a.parentNode.removeChild(a);
+                var d = this.ProcessedUserList[b];
+                if (d != null && !d.ResolvedUser)
+                    this.UnresolvedUserCount--;
+                this.TotalUserCount--;
+                delete this.ProcessedUserList[b];
+                //this.OnControlResolvedUserChanged();
+                //this.OnControlValueChanged()
+            }
+        }
+        function isUserInViewValues(user, view) {
+            for (var i = 0, length = view.length; i < length; i++) {                
+                if (user.Key === view[i].Key) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        function makeUnique(array, fn) {
+            return array.reduce(function (accum, current) {
+                if (typeof fn === "function") {
+                    if (deepIndexOf(accum, current, fn) < 0)
+                        accum.push(current);
+                } else {
+                    if (accum.indexOf(current) < 0) {
+                        accum.push(current);
+                    }
+                }
+                return accum;
+            }, []);
+        }
+        function deepIndexOf(array, searchElement, fn) {
+            if (array == null) {
+                throw new TypeError('"array" is null or not defined');
+            }
+
+            var O = Object(array);
+            var len = O.length
+            var k;
+            if (len === 0) {
+                return -1;
+            }
+            for (k = 0; k < len; k++) {
+                if (fn(searchElement, array[k]))
+                    return k;
+            }
+            return -1;
+        };
+        function compareArray(a, b) {
+            if (a === b)
+                return true;
+            if (angular.isArray(a) && angular.isArray(b)) {
+                if (a.length !== b.length)
+                    return false;
+
+                a.sort(function (a, b) { return a.Key - b.Key });
+                b.sort(function (a, b) { return a.Key - b.Key });
+
+                for (var i = 0, length = a.length; i < length; i++) {
+                    if (a[i].Key !== b[i].Key)
+                        return false;
+                }
+            }
+            return true;
+
+        }
 
         function link(scope, element, attrs, ngModelCtrl) {
+            scope.ngModel = ngModelCtrl.$modelValue || [];
+
             //the Id for the Div that will be used to create the picker
-            var pickerOuterId = element[0].children[0].id;
-            //the Id fo the div where we can find the picker
-            var pickerDivId = pickerOuterId + "_TopSpan";
+            var pickerGeneratorId = element[0].id;
+            //the Id for the div where we can find the picker
+            var pickerDivId = pickerGeneratorId + "_TopSpan";
             var peoplePicker;
 
             // we define a schema object for initilizing the picker.
@@ -34,94 +113,34 @@
             //schema["OnValueChangedClientScript"] = function (elementId, values) {};
 
             // Render and initialize the picker. 
-            // Pass the ID of the DOM element that contains the picker, an array of initial
+            // Pass the ID of the DOM element that contains the picker, an empty array of initial
             // PickerEntity objects to set the picker value, and a schema that defines
             // picker properties.
-            window.SPClientPeoplePicker_InitStandaloneControlWrapper(pickerOuterId, [], schema);
+            window.SPClientPeoplePicker_InitStandaloneControlWrapper(pickerGeneratorId, [], schema);
             // Get the people picker object from the page.
             peoplePicker = window.SPClientPeoplePicker.SPClientPeoplePickerDict[pickerDivId];
-            //console.log(peoplePicker);
-
-            //ngModelCtrl.$modelValue = ngModelCtrl.$modelValue || [];
-            scope.ngModel = ngModelCtrl.$modelValue || [];
-
-            ngModelCtrl.$isEmpty = function (value) {
-                //console.log("cheking for empty");
+            
+            
+            //the definition of empty is in this directive more abstract, being that if its not a array of the datastructure we've defined its empty..
+            ngModelCtrl.$isEmpty = function (value) {            
                 if (angular.isArray(value) && value.length > 0 && (value.map(function (i) { return i.Key; })).length > 0)
                     return false;
                 return true;
             }
-            ngModelCtrl.$formatters.push(function (modelValue) {
-                console.log("formatters", modelValue);
-                //if (angular.isUndefined(modelValue))
-                //    modelValue = "";
-                //if (angular.isArray(modelValue)) {
-                //    return modelValue.map(function (item) {
-                //        return item.Key;
-                //    }).join(";");
-                //}
-                return modelValue; //string of keys
-            });
 
-            //utility functions
-            function DeleteProcessedUser(a) {
-                var e = null;
-                if (a == null) {
-                    var c = document.getElementById(this.ResolvedListElementId);
-                    if (c != null)
-                        a = c.lastChild
-                }
-                if (a != null) {
-                    var b = a.id;
-                    a.parentNode.removeChild(a);
-                    var d = this.ProcessedUserList[b];
-                    if (d != null && !d.ResolvedUser)
-                        this.UnresolvedUserCount--;
-                    this.TotalUserCount--;
-                    delete this.ProcessedUserList[b];
-                    //this.OnControlResolvedUserChanged();
-                    //this.OnControlValueChanged()
-                }
-            }
-            function isUserInViewValues(user, view) {
-                for (var i = 0, length = view.length; i < length; i++) {
-                    console.log(user.Key, view[i].Key)
-                    if (user.Key === view[i].Key) {
-                        return true;
-                    }
-                }
-                return false;
-            }
-            Array.prototype.unique = function (fn) {
-                return this.reduce(function (accum, current) {
-                    if (typeof fn === "function") {
-                        if (accum.deepIndexOf(current, fn) < 0)
-                            accum.push(current);
-                    } else {
-                        if (accum.indexOf(current) < 0) {
-                            accum.push(current);
-                        }
-                    }
-                    return accum;
-                }, []);
-            }
-            Array.prototype.deepIndexOf = function (searchElement, fn) {
-                if (this == null) {
-                    throw new TypeError('"this" is null or not defined');
-                }
+            //ngModelCtrl.$formatters.push(function (modelValue) {
+            //    console.log("formatters", modelValue);
+            //    //if (angular.isUndefined(modelValue))
+            //    //    modelValue = "";
+            //    //if (angular.isArray(modelValue)) {
+            //    //    return modelValue.map(function (item) {
+            //    //        return item.Key;
+            //    //    }).join(";");
+            //    //}
+            //    return modelValue; //string of keys
+            //});
 
-                var O = Object(this);
-                var len = O.length
-                var k;
-                if (len === 0) {
-                    return -1;
-                }
-                for (k = 0; k < len; k++) {
-                    if(fn(searchElement, this[k]))
-                        return k;
-                }
-                return -1;
-            };
+            
             ngModelCtrl.$render = function () {
                 //console.log("in render ", ngModelCtrl.$viewValue);
                 if (peoplePicker) {
@@ -134,7 +153,7 @@
                             var user = peoplePicker.ProcessedUserList[id];
                             return { Key: user.UserInfo.Key, UserContainerElementId: user.UserContainerElementId };
                         });
-                        
+
                         for (var i = 0; i < usersInPicker.length; i++) {
                             if (!isUserInViewValues(usersInPicker[i], view)) {
                                 var docNode = document.getElementById(usersInPicker[i].UserContainerElementId);
@@ -158,8 +177,6 @@
             };
 
             ngModelCtrl.$parsers.push(function (viewValue) {
-                console.log("viewValue: ", viewValue);
-
                 if (ngModelCtrl.$isEmpty(viewValue)) {
                     return viewValue;
                 }
@@ -167,42 +184,16 @@
                     return { Key: i.Key, DisplayText: i.DisplayText };
                 });
 
-            });
-            function compareArray(a, b) {
-                if (a === b)
-                    return true;
-                if (angular.isArray(a) && angular.isArray(b)) {
-                    if (a.length !== b.length)
-                        return false;
-
-                    a.sort(function (a, b) { return a.Key - b.Key });
-                    b.sort(function (a, b) { return a.Key - b.Key });
-
-                    for (var i = 0, length = a.length; i < length; i++) {
-                        if (a[i].Key !== b[i].Key)
-                            return false;
-                    }
-                }
-                return true;
-
-            }
+            });            
             scope.$watch("ngModel", function (val) {
-                console.group("watch");
-                console.log(val);
-                console.log(ngModelCtrl.$modelValue);
-                console.log(ngModelCtrl.$viewValue);
-                console.groupEnd();
-                console.log(compareArray(ngModelCtrl.$modelValue, ngModelCtrl.$viewValue));
                 if (!compareArray(ngModelCtrl.$modelValue, ngModelCtrl.$viewValue)) {
-                    var unique = val.unique(function (a, b) {
+                    var unique = makeUnique(val, function (a, b) {
                         return a.Key === b.Key;
                     });
                     ngModelCtrl.$setViewValue(unique);
                 }
                 ngModelCtrl.$render();
             }, true);
-
-
         }
 
         function template() {
@@ -212,10 +203,9 @@
         return {
             restrict: "E",
             require: "ngModel",
+            replace: true,
             link: link,
             scope: {
-                //allowMulti: "=",
-                //cssWidth: "@",
                 ngModel: "="
             },
             template: template
